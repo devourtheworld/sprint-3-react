1b093282a3a94a21dc37999ed419cdbd74053e7d
zbys1av <zbyslav13@gmail.com>
2021-02-01 17:50:22 +0200

    task 5

--- task-5.js
+++ task-5.js
@@ -1,1 +1,16 @@
 // tasks/sprint-3_5.png
+
+function mapCreator(keys, values){
+    const map = new Map();
+    for (let i of keys){
+        if (typeof values[i-1] === "string"){
+            map.set(i, values[i-1]);
+        }
+    }
+    return map;
+}
+
+const map2 = mapCreator([1,2,3,4,5],["A", "B", "C", "D", "E"]);
+// console.log(mapCreator([1,2,3,4,5],["A", "B", "C", "D", "E"]));
+console.log(map2.size);
+console.log(map2.get(1));
