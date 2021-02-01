dd0ba3d9d9f75e86a55a7667ad32bfe534d52259
zbys1av <zbyslav13@gmail.com>
2021-02-01 16:59:19 +0200

    task 1

--- task-1.js
+++ task-1.js
@@ -3,3 +3,13 @@
 // you must use one of the methods to specify the context of this. It is 
 // forbidden to use any cycle.
 
+function getMin(arr){
+    return Math.min(...arr);
+}
+
+//has to be:
+// function getMin(arr){
+//     return Math.min.apply(null, arr);
+// }
+
+console.log(getMin([7, 5, 6, 7, 8, 99, 113]));
